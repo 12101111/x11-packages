90406198d129d2e0c75cb74fded749f80fb5ee0e
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-22 01:09:01 +0300

    gobject-introspection: depend on glib

--- packages/gobject-introspection/build.sh
+++ packages/gobject-introspection/build.sh
@@ -5,4 +5,4 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.60.2
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gobject-introspection/-/archive/${TERMUX_PKG_VERSION}/gobject-introspection-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ad65d198930230c190996fb58153893cf7e827382026b09d1feae441ece425d9
-TERMUX_PKG_DEPENDS="libiconv"
+TERMUX_PKG_DEPENDS="glib, libiconv"
