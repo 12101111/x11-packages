52f6ca63ceaab7912b1df06426273d9e193f80f8
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-21 22:44:04 +0300

    gobject-introspection: initial commit

---
+++ packages/gobject-introspection/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Projects/GObjectIntrospection
+TERMUX_PKG_DESCRIPTION="Introspection system for GObject-based libraries"
+TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=1.60.2
+TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gobject-introspection/-/archive/${TERMUX_PKG_VERSION}/gobject-introspection-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ad65d198930230c190996fb58153893cf7e827382026b09d1feae441ece425d9
+#TERMUX_PKG_DEPENDS=""
